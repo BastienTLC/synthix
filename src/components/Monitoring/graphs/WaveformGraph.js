349fa45254067d87ec651b630cb5796da328744f
JM-Rib <jeanmarin.ribaric@gmail.com>
2023-11-18 03:27:49 +0100

    optmisation affichage graphe

--- src/components/Monitoring/graphs/WaveformGraph.js
+++ src/components/Monitoring/graphs/WaveformGraph.js
@@ -1,57 +1,46 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
+import { Line } from 'react-chartjs-2';
+// eslint-disable-next-line no-unused-vars
 import { Chart } from 'chart.js/auto';
 
 const WaveformGraph = ({ waveform }) => {
     const chartRef = useRef(null);
 
-    useEffect(() => {
-        const ctx = chartRef.current.getContext('2d');
+    const maxAbsValue = Math.max(...waveform.map(value => Math.abs(value)));
 
-        if (chartRef.current.chart) {
-            chartRef.current.chart.destroy();
-        }
+    // Ajuste le décalage pour que le zéro soit au centre du graphique
+    const offset = maxAbsValue;
 
-        // Si waveform est null, crée un graphique par défaut avec des valeurs à zéro
-            // Trouve la valeur maximale absolue dans les données
-            const maxAbsValue = Math.max(...waveform.map(value => Math.abs(value)));
+    const data = {
+        labels: Array.from({ length: waveform.length }, (_, index) => index),
+        datasets: [{
+            label: 'Waveform',
+            data: waveform.map(value => value + offset),
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            fill: false,
+        }],
+    };
 
-            // Ajuste le décalage pour que le zéro soit au centre du graphique
-            const offset = maxAbsValue;
-
-            const newChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: Array.from({ length: waveform.length }, (_, index) => index),
-                    datasets: [{
-                        label: 'Waveform',
-                        data: waveform.map(value => value + offset),
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1,
-                        fill: false,
-                    }],
-                },
-                options: {
-                    scales: {
-                        x: {
-                            type: 'linear',
-                            position: 'bottom',
-                            max: 256, // Valeur maximale pour l'axe des x
-                        },
-                        y: {
-                            type: 'linear',
-                            min: 0,
-                            max: 1, // Valeur maximale pour l'axe des y
-                        },
-                    },
-                    animation: false, // Désactive l'animation
-                },
-            });
-
-            chartRef.current.chart = newChart;
-    }, [waveform]);
+    const options = {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom',
+                min: 0,
+                max: 256, // Valeur maximale pour l'axe des x
+            },
+            y: {
+                type: 'linear',
+                min: 0,
+                max: 1, // Valeur maximale pour l'axe des y
+            },
+        },
+        animation: false, // Désactive l'animation
+    }
 
     return (
-        <canvas ref={chartRef} width="400" height="100"></canvas>
+        <Line ref={chartRef} data={data} options={options}></Line>
     );
 };
 
