deff5c6c16367d22cda746a2ba2bfbbeeb9902ee
BastienTLC <bastientalec@orange.fr>
2023-11-24 17:02:09 +0100

    drag and drop timeline element

--- src/components/Sequenceur/Note/Note.js
+++ src/components/Sequenceur/Note/Note.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useSynthContext } from '../../../context/SynthContext';
 import './Note.css';
 
--- src/components/Sequenceur/Track/Track.js
+++ src/components/Sequenceur/Track/Track.js
@@ -1,5 +1,5 @@
 // Track.js
-import React, {useState,useEffect} from 'react';
+import React from 'react';
 import Note from '../Note/Note';
 import './Track.css';
 
